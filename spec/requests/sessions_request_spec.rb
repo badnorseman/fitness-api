d02b7187324b3a9e1452321e23128bb8efb4060f
Dan Jensen <dj@fitbird.com>
2015-06-02 09:59:11 -0400

    update session request spec

--- spec/requests/sessions_request_spec.rb
+++ spec/requests/sessions_request_spec.rb
@@ -2,30 +2,16 @@ require "spec_helper"
 
 describe "Session", type: :request do
   describe "when user logs in" do
-    context "with invalid credentials" do
-      before do
-        get(
-          "/api/auth/identity/callback",
-          { auth_key: "agent.smith@matrix.com",
-            password: nil })
-      end
+    before do
+      OmniAuth.config.test_mode = true
+      OmniAuth.config.add_mock(:facebook, { uid: "1234" })
+      Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:facebook]
 
-      it "should respond with status 401" do
-        expect(response.status).to eq(401)
-      end
+      get("/api/auth/facebook/callback")
     end
 
-    context "with valid credentials" do
-      before do
-        get(
-          "/api/auth/identity/callback",
-          { auth_key: "agent.smith@matrix.com",
-            password: "dammit" })
-      end
-
-      it "should respond with status 200" do
-        expect(response.status).to eq(200)
-      end
+    it "should respond with status 200" do
+      expect(response.status).to eq(200)
     end
   end
 
