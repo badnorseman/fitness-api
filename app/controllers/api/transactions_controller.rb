04ce250533c6ed04d14ab94e2e16f8e8f452a26b
Dan Jensen <dj@fitbird.com>
2015-07-12 23:10:24 -0400

    add create to transaction controller

--- app/controllers/api/transactions_controller.rb
+++ app/controllers/api/transactions_controller.rb
@@ -1,3 +1,5 @@
+# Move code to Payment#Create once it is working and
+# delete this controller and it's route.
 module Api
   class TransactionsController < ApplicationController
     skip_after_action :verify_authorized
@@ -9,20 +11,27 @@ module Api
         payment_method_nonce: payment_method_nonce)
 
       if @transaction.success?
-        render json: @transaction, status: :created
+        render json: {}, status: :created
       else
-        render json: { errors: @transaction.errors.full_messages }, status: :unprocessable_entity, location: nil
+        render json: {}, status: :unprocessable_entity, location: nil
+        # render json: { errors: @transaction.errors.full_messages }, status: :unprocessable_entity, location: nil
       end
     end
 
     private
 
     def amount
-      params.fetch(:amount)
+      params[:transaction][:amount]
     end
 
     def payment_method_nonce
-      params.fetch(:payment_method_nonce)
+      params[:transaction][:payment_method_nonce]
+    end
+
+    def transaction_params
+      params.require(:transaction).
+        permit(:amount,
+               :payment_method_nonce)
     end
   end
 end
