cfe948208b23a2a3a39737a01fd8581b7150c6df
Dan Jensen <dj@fitbird.com>
2015-07-29 19:52:50 -0400

    make product name unique

--- db/migrate/20150204155951_create_products.rb
+++ db/migrate/20150204155951_create_products.rb
@@ -1,13 +1,16 @@
 class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
-      t.references :user,        index: true
-      t.string     :name,        null: false
-      t.text       :description, null: false, limit: 500
+      t.references :user,           index: true
+      t.string     :name,           null: false, limit: 50
+      t.string     :uniquable_name, null: false, limit: 50
+      t.text       :description,    null: false, limit: 500
       t.string     :currency
       t.integer    :price
       t.datetime   :ended_at
       t.timestamps
     end
+
+    add_index :products, [:user_id, :uniquable_name], unique: true
   end
 end
