8da020376880f9c8ac394914a9b995d3962927a5
Dan Jensen <dj@fitbird.com>
2015-09-18 16:25:02 +0200

    update session_controller

--- app/serializers/session_serializer.rb
+++ app/serializers/session_serializer.rb
@@ -1,8 +1,13 @@
 class SessionSerializer < ActiveModel::Serializer
   attributes :id,
-             :name,
              :email,
+             :name,
+             :avatar,
              :administrator,
              :coach,
              :token
+
+  def avatar
+    object.avatar.url(:small)
+  end
 end
