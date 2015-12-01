9c120e1f305705557e7be906f9bdf689d2abb838
Dan Jensen <dj@fitbird.com>
2015-11-30 22:17:36 -0500

    update application_controller with cors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,8 @@
 class ApplicationController < ActionController::Base
   include Pundit
   skip_before_action :verify_authenticity_token
-  before_action :restrict_access
   before_action :preflight_check
-  after_action :set_access_control_headers
+  before_action :restrict_access
   after_action :verify_authorized, except: :index
   after_action :verify_policy_scoped, only: :index
 
@@ -16,25 +15,14 @@ class ApplicationController < ActionController::Base
   # Allows access to current_user in serializators.
   serialization_scope :current_user
 
+  def preflight_check
+    headers["Access-Control-Max-Age"] = "1728000"
+    render json: {}
+  end
+
   def restrict_access
     authenticate_or_request_with_http_token do |token, options|
       @current_user = User.find_by(token: token)
-    end
-  end
-
-  def set_access_control_headers
-    response.headers["Access-Control-Allow-Origin"] = "*"
-    response.headers["Access-Control-Allow-Methods"] = "GET, OPTIONS, POST"
-    response.headers["Access-Control-Allow-Headers"] = "Accept, Authorization, Auth-Token, auth_token, Content-Type, Email, Origin, Token, X-CSRF-Token, X-Requested-With"
-    response.headers["Access-Control-Max-Age"] = "1728000"
-  end
-
-  def preflight_check
-    if request.method == "OPTIONS"
-      response.headers["Access-Control-Allow-Origin"] = "http://localhost"
-      response.headers["Access-Control-Allow-Methods"] = "GET, OPTIONS, POST"
-      response.headers["Access-Control-Allow-Headers"] = "Accept, Authorization, Auth-Token, auth_token, Content-Type, Email, Origin, Token, X-CSRF-Token, X-Requested-With"
-      response.headers["Access-Control-Max-Age"] = "1728000"
     end
   end
 end
