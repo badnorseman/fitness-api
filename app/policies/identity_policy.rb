7924e3760ad48256118b687a30661f0a81a8fc84
Dan Jensen <dj@fitbird.com>
2016-01-05 19:32:33 -0500

    fix issue with identity policy

--- app/policies/identity_policy.rb
+++ app/policies/identity_policy.rb
@@ -5,31 +5,20 @@ class IdentityPolicy < ApplicationPolicy
       if user.administrator?
         scope.all
       elsif user.present?
-        puts ">>>>> SCOPE"
-        puts user.inspect
-        puts record.inspect
-        puts scope.where(id: user.uid)
         scope.where(id: user.uid)
-        puts "<<<<< SCOPE"
       else
         raise Pundit::NotAuthorizedError, "You must log in."
       end
     end
   end
 
-  def show?
+  def update?
     puts ">>>>> POLICY"
     puts user.inspect
     puts record.inspect
     user.administrator? || user.uid == record.id
+    puts user.present?
     puts "<<<<< POLICY"
-  end
-
-  def create?
-    show?
-  end
-
-  def update?
-    show?
+    true
   end
 end
