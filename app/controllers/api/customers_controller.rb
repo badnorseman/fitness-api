0a9db398e50c656d1cfbdace3f1a1fd8ba5fb7cf
Dan Jensen <dj@fitbird.com>
2016-01-11 18:34:17 -0500

    add policy for Customer

--- app/controllers/api/customers_controller.rb
+++ app/controllers/api/customers_controller.rb
@@ -1,10 +1,8 @@
 module Api
   class CustomersController < ApplicationController
-    skip_after_action :verify_authorized, only: :index
-
     # GET /customers.json
     def index
-      render json: Customer.data_for_listing, status: :ok
+      render json: policy_scope(Customer).data_for_listing, status: :ok
     end
   end
 end
