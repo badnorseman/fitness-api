1019db64aef5bf081d8cb62deeed2cb3c1516927
Dan Jensen <dj@fitbird.com>
2016-01-12 09:11:38 -0500

    add policy for Coach

--- app/policies/customer_policy.rb
+++ app/policies/customer_policy.rb
@@ -4,27 +4,21 @@ class CustomerPolicy < ApplicationPolicy
     def resolve
       if user.administrator?
         scope.all
-      elsif user.coach?
-        scope.all
       else
         raise Pundit::NotAuthorizedError, "You must log in."
       end
     end
   end
 
-  def show?
-    user.administrator? || user.id == record.user_id
-  end
-
   def create?
-    show?
-  end
-
-  def update?
     user.administrator?
   end
 
+  def update?
+    create?
+  end
+
   def destroy?
-    update?
+    create?
   end
 end
